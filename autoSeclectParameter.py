51f10168ccf95d1a9e616b931b291b0b620aeeb4
jiaruncao <32667137+jiaruncao@users.noreply.github.com>
2017-10-21 09:08:26 -0500

    Add files via upload
    自动选择各项参数，将最优参数存入parameter.txt

---
+++ autoSeclectParameter.py
@@ -0,0 +1,68 @@
+
+# coding: utf-8
+
+# In[28]:
+
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+import os
+
+
+# In[30]:
+
+
+knn = KNeighborsClassifier()
+for r,_,files in os.walk('splitData/'):
+    for file in files:
+        data = pd.read_csv(os.path.join('splitData/',file))
+        location = data['longitude']
+        location = pd.DataFrame(location)
+        location.insert(column= 'latitude',value=data['latitude'],loc = 0)
+        location= np.array(location)
+        label = pd.DataFrame(data['shop_id'])
+        label = np.array(label)
+        knn = KNeighborsClassifier()
+        label = label.reshape(label.shape[0],)
+        k_range = list(range(1,20))
+        leaf_range = list(range(1,2))
+        weight_options = ['uniform','distance']
+        algorithm_options = ['auto','ball_tree','kd_tree','brute']
+        param_gridknn = dict(n_neighbors = k_range,weights = weight_options,algorithm=algorithm_options,leaf_size=leaf_range)
+        gridKNN = GridSearchCV(knn,param_gridknn,cv=3,scoring='accuracy',verbose=1)
+        gridKNN.fit(location,label)
+        fr = open('parameter.txt','a')
+        fr.write(str(gridKNN.best_score_)+'\n')
+        fr.write(str(gridKNN.best_params_)+'\n')
+        fr.write('\n')
+        fr.close()
+        
+
+
+# In[27]:
+
+
+
+
+
+# In[20]:
+
+
+mydata
+
+
+# In[19]:
+
+
+location = mydata['longitude']
+
+
+# In[ ]:
+
+
+
+
