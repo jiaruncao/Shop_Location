6a259451de4dfbd85c6a4102f4d002832904d226
jiaruncao <32667137+jiaruncao@users.noreply.github.com>
2017-10-27 22:37:37 -0500

    Update fifth_knn_submit.py

--- fifth_knn_submit.py
+++ fifth_knn_submit.py
@@ -6,10 +6,10 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import GridSearchCV
+#from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+#from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 
 
 # In[26]:
